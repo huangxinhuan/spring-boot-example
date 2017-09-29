c787f53de7e1bc4fd8a0d651037879f88b0e4171
V1cent_zZ <h_xh@foxmail.com>
2017-09-29 16:31:03 +0800

    add demo-07-springDataJpa

--- demo-06-taskScheduling/src/main/java/com/example/demo/ScheduledTask.java
+++ demo-06-taskScheduling/src/main/java/com/example/demo/ScheduledTask.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class ScheduledTask {
 
     private static final Logger log = LoggerFactory.getLogger(TaskSchedulingApp.class);
